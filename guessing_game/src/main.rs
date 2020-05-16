e945ec0f22b0343f3972198d621452a6f49c218a
YasushiKobayashi <ptpadan@gmail.com>
2020-05-17 03:45:24 +0900

    add unit test

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 3);
 
     loop {
         println!("Please input your guess.");
@@ -22,14 +22,36 @@ fn main() {
         };
 
         println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        let res = guess_secret_number(guess, secret_number);
+        if res {
+            break;
         }
     }
+}
+
+fn guess_secret_number(guess: u32, secret_number: u32) -> bool {
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            return true;
+        }
+    }
+    return false;
+}
+
+#[test]
+fn guess_secret_number_same() {
+    let res = guess_secret_number(1, 1);
+    assert_eq!(res, true);
+}
+
+#[test]
+fn guess_secret_number_not_same() {
+    let large = guess_secret_number(1, 2);
+    assert_eq!(large, false);
+
+    let small = guess_secret_number(2, 1);
+    assert_eq!(small, false);
 }
